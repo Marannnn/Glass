a143d4ea4849e4eeee61424c96c87bd06390726c
Martin Hrubes <martin.hrubes1@seznam.cz>
2025-06-23 14:39:57 +0200

    readme

--- Glass-Wine_GUI/Glass/obj/Debug/net9.0/Glass.AssemblyInfo.cs
+++ Glass-Wine_GUI/Glass/obj/Debug/net9.0/Glass.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Glass")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e30b33c5297446f02c3027ef459e1fe7f8f8e28c")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6008100818f54b13a663620e307ccde2ad798f7e")]
 [assembly: System.Reflection.AssemblyProductAttribute("Glass")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Glass")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
---
+++ readme.md
@@ -0,0 +1,22 @@
+# Glass- wine manager
+
+## About
+Simple easy-to-use wine manager.
+
+Easly manage wine prefixes and programs with user library.
+
+## Screenshots
+![main page-applications](./preview/main-applications.png)
+Main application page
+
+![main page-prefixes](./preview/main-prefixes.png)
+Main prefix page
+
+![new program window](./preview/new_program.png)
+Adding new program window
+
+![installer window](./preview/installer.png)
+When using installer
+
+![new prefix window](./preview/new_prefix.png)
+Adding new prefix window
